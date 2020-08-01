06f23463cc4babfb02f0547891dd408c980ba2a3
savip <pratapadhikari81@gmail.com>
2020-08-01 20:52:30 +0930

    Updates

---
+++ precache-manifest.b70182aaa5dfd13fc894c16013ace7d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24ea01abe3849d82a0f074ae7ebe1792",
+    "url": "/monsterreact/index.html"
+  },
+  {
+    "revision": "da90965a4c5a23204d1c",
+    "url": "/monsterreact/static/css/main.3f2b1f40.chunk.css"
+  },
+  {
+    "revision": "e42c151a64de41a4c6b2",
+    "url": "/monsterreact/static/js/2.f67ffe95.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsterreact/static/js/2.f67ffe95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da90965a4c5a23204d1c",
+    "url": "/monsterreact/static/js/main.d4afdf6e.chunk.js"
+  },
+  {
+    "revision": "4438d0505fe1b9d9292c",
+    "url": "/monsterreact/static/js/runtime-main.75f1038e.js"
+  }
+]);
